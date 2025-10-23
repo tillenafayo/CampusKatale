4f62814d2139d1133f155c5982e4856a82433ddd
Reellz <atwanbwambs@gmail.com>
2025-10-23 15:48:57 +0300

    Add Authentication and implement protected routes

--- CampusKatale-Client/src/App.jsx
+++ CampusKatale-Client/src/App.jsx
@@ -56,15 +56,3 @@ function App() {
 }
 
 export default App;
-import {Routes, Route,} from "react-router-dom";
-
-function App(){
-  <Routes>
-    <Route path="/" element={<Home/>}/>
-    <Route path="/login" element={<Login/>}/>
-    <Route path="/signup" element={<SignUp/>}/>
-    <Route path="/marketplace" element={<Marketplace/>}/>
-  </Routes>
-}
-
-export default App;
--- CampusKatale-Client/src/main.jsx
+++ CampusKatale-Client/src/main.jsx
@@ -21,7 +21,7 @@ createRoot(document.getElementById("root")).render(
         <BrowserRouter>
           <App />
         </BrowserRouter>
+      </ClerkProvider>
       </MantineProvider>
-    </ClerkProvider>
   </StrictMode>
 );
